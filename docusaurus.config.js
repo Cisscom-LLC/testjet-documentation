125473710be0db0fa78315535741c0f30fa40f5a
Nurul Ferdous <nferdous@thetitantech.com>
2023-04-12 12:31:22 +0600

    search added

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -78,7 +78,7 @@ const config = {
           },
           {to: '/blog', label: 'Blog', position: 'left'},
           {
-            href: '/contact',
+            href: '/docs/intro',
             label: 'Submit a ticket',
             position: 'right',
           },
